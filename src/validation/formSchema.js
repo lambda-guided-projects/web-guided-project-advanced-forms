7233d744ed5d02b0857b864e6f302d369f881bf5
Andreas Bolos <andreasbolos@Andreass-MacBook-Pro.local>
2021-02-24 11:19:07 -0700

    wip

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -1,1 +1,18 @@
 // Here goes the schema for the form
+import * as yup from 'yup'
+
+const formSchema = yup.object().shape({
+    username: yup.string()
+        .required('Username is required, please fill out.'),
+    email: yup.string()
+        .email('Must be a valid email address')
+        .required('Email is required'),
+    role: yup.string()
+    .oneOf(['student', 'instructor', 'alumni', 'tl'], 'Role is required'),
+    civil: yup.string()
+        .oneOf(['married', 'single'], 'Civil status is required'),
+   // Checkboxes are not required
+    hiking: yup.boolean(),
+    reading: yup.boolean(),
+    coding: yup.boolean()
+})
