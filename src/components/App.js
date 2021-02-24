cd38dc2e5f830a6ea0b33dbee2e920ab4fbc126e
Andreas Bolos <andreasbolos@Andreass-MacBook-Pro.local>
2021-02-24 11:53:14 -0700

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -117,7 +117,7 @@ export default function App() {
   useEffect(() => {
     // 🔥 STEP 9- ADJUST THE STATUS OF `disabled` EVERY TIME `formValues` CHANGES
     formSchema.isValid(formValues).then(valid => setDisabled(!valid))
-  }, [])
+  }, [formValues])
 
   return (
     <div className='container'>
